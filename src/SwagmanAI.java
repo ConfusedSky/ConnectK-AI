3760692dc470c57c5405b1352524161207849c1a
Masa Maeda <masamaedae@gmail.com>
2016-10-23 23:39:51 -0700

    First turn in

--- src/SwagmanAI.java
+++ src/SwagmanAI.java
@@ -1,34 +1,215 @@
 import java.awt.Point;
-import java.util.Random;
 
 import connectK.BoardModel;
 import connectK.CKPlayer;
+import connectK.ConnectKGUI;
 
 public class SwagmanAI extends CKPlayer {
 	
-	Random r = new Random();
+	private class GameNode
+	{
+		public BoardModel state;
+		public int score = 0;
+		
+		public GameNode( BoardModel bm )
+		{
+			state = bm;
+		}
+	}
+	
+	// True if player is first player false otherwise
+	Boolean p;
 
 	public SwagmanAI(byte player, BoardModel state) {
 		super(player, state);
 		teamName = "Swagman";
+		p = player == 1;
 	}
 
 	@Override
-	public Point getMove(BoardModel state) {
+	public Point getMove(BoardModel state) 
+	{
+		//System.out.println( scoreBoard(state, p) );
+		Point poi = recurseBestMove( new GameNode(state), 3, true );
+		//System.out.println( scoreBoard(state.placePiece(poi, player), p ) );
+		return poi;
+	}
+	
+	private Point recurseBestMove( GameNode node, int layer, boolean playing )
+	{
+		Point none = new Point( -1, -1 );
+		Point move = new Point();
+		int bestScore;
+		GameNode nodePrime;
 		
-		Point choice = new Point(0,0);
-		
-		do
+		// If the playing player can win
+		if( !(move = CanWin( node.state, !(playing ^ p) )).equals(none)  )
 		{
-			choice.x = r.nextInt(state.getWidth());
-			choice.y = r.nextInt(state.getHeight());
-		} while( state.getSpace(choice) != 0 );
+			node.score = ((playing)?(Integer.MAX_VALUE-1):Integer.MIN_VALUE+1);
+		}
+		// If the player that isn't playing can block a win
+		else if( !(move = CanWin( node.state, (playing ^ p) )).equals(none ) )
+		{
+			// blocking a win is second only to winning
+			node.score = ((playing)?(Integer.MAX_VALUE-2):Integer.MIN_VALUE+2);
+		}
+		// If we hit the end layer calculate the position current positions value
+		else if( layer == 0 )
+		{
+			node.score = scoreBoard( node.state, p );
+		}
+		else if( playing )
+		{
+			bestScore = Integer.MIN_VALUE;
+			for( int i = 0; i < node.state.getWidth(); i++ )
+			{
+				// make sure that it only checks the top row if gravity is enabled
+				for( int j = node.state.getHeight()-1; (!node.state.gravityEnabled() || j == node.state.getHeight()-1) && j >= 0; j-- )
+				{
+					if( node.state.getSpace(i, j) == 0 )
+					{
+						// Generate a move
+						Byte token = (byte)(p?1:2);
+						nodePrime = new GameNode( node.state.placePiece(new Point(i,j), token) );
+						
+						// Recurse from the new move
+						recurseBestMove( nodePrime, layer-1, false );
+						
+						if( nodePrime.score > bestScore )
+						{
+							move = new Point(i,j);
+							bestScore = nodePrime.score;
+						}
+					}
+				}
+				
+				node.score = bestScore;
+			}
+		}
+		// else not playing
+		else
+		{
+			bestScore = Integer.MAX_VALUE;
+			for( int i = 0; i < node.state.getWidth(); i++ )
+			{
+				// make sure that it only checks the top row if gravity is enabled
+				for( int j = node.state.getHeight()-1; (!node.state.gravityEnabled() || j == node.state.getHeight()-1) && j >= 0; j-- )
+				{
+					if( node.state.getSpace(i, j) == 0 )
+					{
+						// Generate a move
+						Byte token = (byte)(!p?1:2);
+						nodePrime = new GameNode( node.state.placePiece(new Point(i,j), token) );
+						
+						// Recurse from the new move
+						recurseBestMove( nodePrime, layer-1, false );
+						
+						if( nodePrime.score < bestScore )
+						{
+							move = new Point(i,j);
+							bestScore = nodePrime.score;
+						}
+					}
+				}
+				
+				node.score = bestScore;
+			}
+		}
 		
-		return choice;
+		return move;
 	}
 
 	@Override
 	public Point getMove(BoardModel state, int deadline) {
 		return getMove(state);
 	}
+	
+	
+	// Checks to see if the current player can win 
+	// if they can't, return -1, -1 for false
+	// if they can return the move they can take to win
+	private Point CanWin( BoardModel state, boolean currentPlayer )
+	{
+		BoardModel statePrime;
+		for( int i = 0; i < state.getWidth(); i++ )
+		{
+			// make sure that it only checks the top row if gravity is enabled
+			for( int j = state.getHeight()-1; (!state.gravityEnabled() || j == state.getHeight()-1) && j >= 0; j-- )
+			{
+				if( state.getSpace(i, j) == 0 )
+				{
+					statePrime = state.placePiece(new Point(i,j), (byte)(currentPlayer?1:2));
+					if( statePrime.winner() == (byte)(currentPlayer?1:2))
+					{
+						return new Point(i,j);
+					}
+				}
+			}
+		}
+		
+		return new Point(-1,-1);
+	}
+	
+	public static int scoreBoard(BoardModel state, Boolean player)
+	{
+		// Determine actual score
+		int score = 0;
+		
+		int[][] limits = 
+		{
+			// i lower, i upper, j lower, j upper, k multiplier for i, k multiplier for j
+			{ 0, state.getWidth(), 0, state.getHeight() - state.getkLength() + 1,  0, 1 }, // vertical
+			{ 0, state.getWidth() - state.getkLength() + 1, 0, state.getHeight(), 1, 0 }, // horizontal
+			{ 0, state.getWidth() - state.getkLength() + 1, 0, state.getHeight() - state.getkLength() + 1, 1, 1 }, // upright
+			{ 0, state.getWidth() - state.getkLength() + 1, state.getkLength() - 1, state.getHeight(), 1, -1 } // downright
+		};
+		
+		byte mytoken = player?(byte)1:(byte)2;
+		byte value;
+		byte token;
+		
+		int i, j, k, l;
+		for( l = 0; l < 4; l++ )
+		{
+			for( i = limits[l][0]; i < limits[l][1]; i++ )
+			{
+				for( j = limits[l][2]; j < limits[l][3]; j++ )
+				{
+					token = (byte)0;
+					for( k = 0; k < state.getkLength(); k++ )
+					{
+						if( (value = state.getSpace(i+k*limits[l][4],j+k*limits[l][5])) != token )
+						{
+							if( token == (byte) 0 )
+							{
+								token = value;
+							}
+							else if( value != 0 )
+							{
+								break;
+							}
+						}
+					}
+					if( k == state.getkLength() && token != (byte)0 )
+					{
+						score += (mytoken == token)?(1):(-1);
+					}
+				}
+			}
+		}
+		
+		return score;
+	}
+	
+//	public static void main( String[] args )
+//	{
+//		try {
+//			args = new String[]{"C:\\Users\\Masa\\Google Drive\\Fall 2016\\ICS 171\\Project\\ConnectKEclipse\\bin\\SwagmanAI.class"};
+//			ConnectKGUI.main(args);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+	
 }
